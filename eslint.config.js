2719bf1bdced14f64584ba2d47f06c4c9436c33f
RobertKafka <62625934+RobertKafka@users.noreply.github.com>
2025-03-22 23:59:29 -0500

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,13 @@
-{
+module.exports = {
+  // Extending recommended configurations for Next.js
   "extends": ["next", "next/core-web-vitals"],
   "rules": {
-    // your custom rules
+    // Define your custom rules here
+    // Example: "semi": ["error", "always"]
   },
+  // Including custom ESLint plugins
   "plugins": ["./custom_rules/"],
   "linterOptions": {
     "reportUnusedDisableDirectives": true
   }
-}
+};
