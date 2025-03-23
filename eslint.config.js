df715c363e4c6488e2cb3618c80b3675d918eacc
RobertKafka <62625934+RobertKafka@users.noreply.github.com>
2025-03-22 23:50:41 -0500

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+{
+  "extends": ["next", "next/core-web-vitals"],
+  "rules": {
+    // your custom rules
+  },
+  "plugins": ["./custom_rules/"],
+  "linterOptions": {
+    "reportUnusedDisableDirectives": true
+  }
+}
